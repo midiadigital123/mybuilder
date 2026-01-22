9e315a55de91c407bedd810a1cd2d893a14e85b2
Bernard Rodrigues <bernard.rodrigues@outlook.com>
2026-01-22 15:53:37 -0300

    refactor: ajuste de idioma

--- my-builder/src/utils/util.js
+++ my-builder/src/utils/util.js
@@ -1,7 +1,8 @@
 /**
  * 
  * @param {object} element 
- * @returns {number} number of characters in the element if it is a text element, otherwise 0
+ * @returns {number} número de caracteres em um elemento se este elemento for um elemento de texto.
+ * Do contrário gera erro no console e retorna 0.
 */
 const elementCharCounter = (element) => {
     const textTagNames = ["P", "SPAN", "H1", "H2", "H3", "H4", "H5", "H6"];
@@ -9,6 +10,6 @@ const elementCharCounter = (element) => {
     if(textTagNames.includes(element.tagName)) {
         return element.textContent.length;
     }
-    console.error("Provided element is not a text element.");
+    console.error("Elemento de entrada não é um elemento de texto");
     return 0;
 }
